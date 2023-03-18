b08d02faa5f04df8edd85366ba4342ba1db00861
LxstInTheWoods <soltober7764@icloud.com>
2023-03-18 06:16:28 -0500

    yuh

--- js/topbar.js
+++ js/topbar.js
@@ -8,9 +8,16 @@ for (const value of topbar.getElementsByTagName("h1"))
     {
         value.addEventListener("mouseenter", () =>{
             value.animate([{color:"#7289da"}], {duration:150, fill:"forwards"})
+            value.parentElement.children[1].style.visibility = "visible"
+            value.parentElement.children[1].animate([{"width":"100%"}], {duration:150, fill:"forwards"})
+
         })
         value.addEventListener("mouseleave", () =>{
             value.animate([{color:"white"}], {duration:150, fill:"forwards"})
+            value.parentElement.children[1].animate([{"width":"0%"}], {duration:150, fill:"forwards"})
+            setTimeout(() => {
+                value.parentElement.children[1].style.visibility = "hidden"
+            }, 153);
         })
 
         value.addEventListener("click", ()=>{
