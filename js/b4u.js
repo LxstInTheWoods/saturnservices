4851b8a252a17838276c12cbf7080734fec6f188
LxstInTheWoods <soltober7764@icloud.com>
2023-03-23 06:32:19 -0500

    moboil test

--- js/b4u.js
+++ js/b4u.js
@@ -4,6 +4,10 @@ const storageclone = document.getElementById('addonstorage')
 const addonHolder = document.getElementById('addonholder')
 const addtocart = document.getElementById("additemtocart")
 
+var totalorder = {
+
+}
+
 
 
 
@@ -55,10 +59,8 @@ addtocart.addEventListener('click', ()=>{
         ],
 
     }
-    const itemName = document.getElementById("itemname")
-    const itemPrice = document.getElementById("itemprice")
-
-    console.log(itemName.children)
+    const itemName = document.getElementById("itemname_ZXX")
+    const itemPrice = document.getElementById("itemprice_ZXX")
 
     item["name"] = itemName.children[2].value
     item["price"] = itemPrice.children[2].value
@@ -77,5 +79,67 @@ addtocart.addEventListener('click', ()=>{
     }
 
     coursemod.animate([{transform:"translateX(-400%"}], {duration:150, fill:"forwards"})
-    //code to clear form here
+
+    totalorder[Object.keys(totalorder).length] = item
+
+
+    //CLONE LISTITEMTHING
+
+
+    async function internalscopeDD4CA()
+    {
+        const clone = document.getElementById("ORDERITEM").cloneNode(true)
+
+        const clonemod = clone.firstChild.parentElement
+
+        const delbutton = clonemod.children[0].children[0]
+        
+        const itemnameprice = clonemod.children[0].children[1].children[0]
+
+        const addonholder = clonemod.children[0].children[1].children[2]
+        
+        const addon = clonemod.children[0].children[1].children[2].children[0]
+
+        delbutton.addEventListener("click", ()=>{
+            clone.remove()
+        })
+
+        itemnameprice.style.color = "white"
+        itemnameprice.style.fontFamily = 'Arial, Helvetica, sans-serif'
+        itemnameprice.textContent = `${item['name']} • $${item['price']}`
+
+        for (const crv9 of item.addons)
+        {
+            const aclone = addon.cloneNode(true)
+            aclone.textContent = `Addon: ${crv9}`
+            aclone.style.display = "block"
+            addonholder.appendChild(aclone)
+        }
+
+        
+
+        document.getElementById('ORDERITMSHOLD').appendChild(clone)
+    }    internalscopeDD4CA()
+
+    //rest
+
+
+
+    
+    //clear IN/IP
+  for(const v4cz of document.getElementsByClassName('iteminfo') ){ if(v4cz.id.includes("_ZXX")){for(const v5cz of v4cz.getElementsByTagName("textarea")){v5cz.value=""}}}
+
+  //clear addons
+    const addholdchildren = document.getElementById("addonholder").children
+
+    //try to figure out how the fuck this fixed it later. spent 2 hours trying to fix this crap.
+    //origin problem: it was leaving an element instead of deleting them all.
+    for (let k = addholdchildren.length - 1; k > 0; k--)
+    {
+        addholdchildren[k].remove()
+    }
+
+    
+    //end
+
 })
