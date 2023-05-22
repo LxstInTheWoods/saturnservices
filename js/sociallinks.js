217ed4535d0773c328fc73e0942490e1cb76f842
LxstInTheWoods <soltober7764@icloud.com>
2023-05-22 09:22:15 -0500

    n

--- js/sociallinks.js
+++ js/sociallinks.js
@@ -1,14 +1,20 @@
 function social(x)
 {
     const data = {
-        discord:["https://discord.gg/UYsdygmX"],
+        discord:[""],
         telegram:["https://t.me/+eOkB8WnJ5n40NDNh"],
         amazon:["./amazon.html", "_self"],
         home:["./index.html", "_self"],
         b4u:["./b4u.html", "_self"]
     }
-
+    try
+    {
     return `${data[x.toLowerCase()][0]}|${data[x.toLowerCase()][1]}`
+    }catch(err){
+        alert("Unavailable")
+        console.warn(err)
+        return  
+    }
 }
 
 
@@ -24,6 +30,12 @@ for (const element of document.getElementsByClassName("sociallink"))
         element.animate([{borderColor:"white"}], {duration:150, fill:"forwards"})
     })
     element.addEventListener("click", () =>{
+        //fix later
+        if (social(element.id).split("|")[1].length < 1)
+        {
+            alert("Unavailable")
+            return
+        }
         window.open(social(element.id).split("|")[0], social(element.id).split("|")[1])
     })
 }
