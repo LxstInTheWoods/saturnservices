83b24b986c2844723517bae5403ac95596272317
LxstInTheWoods <68558053+LxstInTheWoods@users.noreply.github.com>
2023-11-19 22:38:22 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -58,6 +58,25 @@ v.addEventListener("mouseleave", function(){
 })
 }
 
+for (const x of [...document.getElementsByClassName("prefregion")]){
+    x.addEventListener("mouseenter", ()=>{
+        x.animate([{border:"solid white 2px"}], {duration:250, fill:'forwards'})
+    })
+
+    x.parentElement.addEventListener("mouseenter", ()=>{
+        
+         x.parentElement.animate([{border:"solid white 2px"}], {duration:250, fill:'forwards'})
+    })
+    
+    x.parentElement.addEventListener("mouseleave", ()=>{
+        x.parentElement.animate([{border:"solid #2c2c2c 2px"}], {duration:250, fill:'forwards'})
+   })
+    x.addEventListener("mouseleave", ()=>{
+        x.animate([{border:"solid #2c2c2c 2px"}], {duration:250, fill:'forwards'})
+
+    })
+
+}
 const dnld = document.getElementsByClassName("order")[0]
 dnld.addEventListener("mouseenter", ()=>{
 dnld.animate([{boxShadow:"0 0 10px white, 0 0 30px white, 0 0 50px white"}], {duration:150, fill:"forwards"})
