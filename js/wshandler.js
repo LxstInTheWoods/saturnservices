ff0f5ed33acd31752d68c421496b494fe74a177e
LxstInTheWoods <soltober7764@icloud.com>
2024-08-21 21:54:00 -0500

    test

--- js/wshandler.js
+++ js/wshandler.js
@@ -1,21 +1,10 @@
 import * as utils from "./modules.js"
-const wsUrl = 'wss://api.terminalsaturn.com:1111';
+const wsUrl = 'wss://api.terminalsaturn.com:445';
 const socket = new WebSocket(wsUrl);
-
+console.log(socket)
 export var SID
 
-export async function awaitSID() {
-    for (let k = 0; k < 20; k++) {
-        if (k > 20) {
-            utils.generateNotification("System", "Could not connect to server. Please refresh to try again.")
-            return "F"
-        }
-        if (SID) {
-            return SID
-        }
-        await utils.delay(1)
-    }
-}
+
 window.addEventListener('load', function () {
 socket.onopen = function (event) {
     socket.send(JSON.stringify({ type: '01', message: utils.getUserData() }));
@@ -57,4 +46,16 @@ socket.onerror = function (error) {
     console.error('WebSocket error:', error.message);
 };
 })
+export async function awaitSID() {
+    for (let k = 0; k < 20; k++) {
+        if (k > 20) {
+            utils.generateNotification("System", "Could not connect to server. Please refresh to try again.")
+            return "F"
+        }
+        if (SID) {
+            return SID
+        }
+        await utils.delay(1)
+    }
+}
 //make socket connections require password when connecting via username - security/bug
